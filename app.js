8f272920f810bb2a6dd4093d06f80f06113f9465
Raj Thombare (Rakaa) <therajthombare@gmail.com>
2021-08-03 00:46:26 +0530

    feat: add show section if valid input

--- app.js
+++ app.js
@@ -2,8 +2,22 @@ var dateOfBirth = document.querySelector("#date-of-birth");
 var luckyNumber = document.querySelector("#lucky-number");
 var btnCheck = document.querySelector("#btn-check");
 var output = document.querySelector("#output-div");
+var showDiv = document.querySelectorAll(".show-div");
+var btnContinue = document.querySelector("#btn-continue");
 
 btnCheck.addEventListener("click", checkBirthdateIsLucky);
+btnContinue.addEventListener("click", continueBtnHandler);
+
+function continueBtnHandler(){
+    let birthdayDate = dateOfBirth.value;
+    if(birthdayDate){
+        showDiv[0].style.display = "block";
+        showDiv[1].style.display = "block";
+    }else{
+        alert("Please enter Birthdate to continue!!")
+    }
+    
+}
 
 function checkBirthdateIsLucky(){
     const dob = dateOfBirth.value;
@@ -11,9 +25,9 @@ function checkBirthdateIsLucky(){
     const luckNum = luckyNumber.value;
     
     if(sum%luckNum===0){
-        output.innerText = "Your Birthday is Lucky";
+        output.innerText = "Your Birthdate is Lucky";
     }else{
-        output.innerText = "Your Birthday is not Lucky";
+        output.innerText = "Your Birthdate is not Lucky";
     }
 }
 
